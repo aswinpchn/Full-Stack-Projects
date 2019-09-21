9b4c5b3bc3425979a9668653bbabc290bbf38a95
Aswin <aswin-pc@Aswins-MacBook-Air.local>
2019-09-20 21:20:39 -0700

    Credentials : true; Otherwise ir issued CORS error in client.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var cors = require('cors');
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: false }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Credentials', 'false');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
     res.setHeader('Cache-Control', 'no-cache');
