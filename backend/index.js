9400c849600b0b7bdb9e93917e223785004635fc
Aswin <aswin-pc@MacBook-Pro.local>
2019-12-08 16:49:39 -0800

    Backend - Working

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-//const schema = require('./schema/standAloneBookschema');
-//const schema = require('./schema/complexBookschema');
-const schema = require('./schema/BookWithMutationSchema');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+var cors = require('cors')
+// const schema = require('./schema/standAloneBookschema');
+// const schema = require('./schema/complexBookschema');
+// const schema = require('./schema/BookWithMutationSchema');
+const schema = require('./schema/grubhubGraphQLSchema');
 
-var app = express();
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 app.use('/graphql', graphqlHTTP({
-    schema, // ES6 so if name same, one time writing is enough
-    graphiql : true // This makes this URL as UI like thing to test queries
+  schema, // ES6 so if name same, one time writing is enough
+  graphiql: true, // This makes this URL as UI like thing to test queries
 }));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+app.listen(8080, () => console.log('Now browse to localhost:8080/graphql'));
