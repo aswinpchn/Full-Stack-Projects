e0e64501cf0ce62e5157177b40131185f212f449
Aswin <aswin-pc@MacBook-Pro.local>
2020-03-02 00:19:44 -0800

    Modified tab indents

--- src/main/java/edu/sjsu/cmpe275/aop/tweet/aspect/RetryAspect.java
+++ src/main/java/edu/sjsu/cmpe275/aop/tweet/aspect/RetryAspect.java
@@ -8,25 +8,25 @@ import org.aspectj.lang.annotation.Around;
 @Aspect
 @Order(1)
 public class RetryAspect {
-    /***
-     * Following is a dummy implementation of this aspect.
-     * You are expected to provide an actual implementation based on the requirements, including adding/removing advices as needed.
-     * @throws Throwable 
-     */
+  /***
+   * Following is a dummy implementation of this aspect.
+   * You are expected to provide an actual implementation based on the requirements, including adding/removing advices as needed.
+   * @throws Throwable
+   */
 
-	@Around("execution(public int edu.sjsu.cmpe275.aop.tweet.TweetService.*tweet(..))")
-	public int dummyAdviceOne(ProceedingJoinPoint joinPoint) throws Throwable {
-		System.out.printf("Prior to the executuion of the metohd %s\n", joinPoint.getSignature().getName());
-		Integer result = null;
-		try {
-			result = (Integer) joinPoint.proceed();
-			System.out.printf("Finished the executuion of the metohd %s with result %s\n", joinPoint.getSignature().getName(), result);
-		} catch (Throwable e) {
-			e.printStackTrace();
-			System.out.printf("Aborted the executuion of the metohd %s\n", joinPoint.getSignature().getName());
-			throw e;
-		}
-		return result.intValue();
-	}
+  @Around("execution(public int edu.sjsu.cmpe275.aop.tweet.TweetService.*tweet(..))")
+  public int dummyAdviceOne(ProceedingJoinPoint joinPoint) throws Throwable {
+    System.out.printf("Prior to the executuion of the metohd %s\n", joinPoint.getSignature().getName());
+    Integer result = null;
+    try {
+      result = (Integer) joinPoint.proceed();
+      System.out.printf("Finished the executuion of the metohd %s with result %s\n", joinPoint.getSignature().getName(), result);
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.out.printf("Aborted the executuion of the metohd %s\n", joinPoint.getSignature().getName());
+      throw e;
+    }
+    return result.intValue();
+  }
 
 }
--- src/main/java/edu/sjsu/cmpe275/aop/tweet/aspect/ValidationAspect.java
+++ src/main/java/edu/sjsu/cmpe275/aop/tweet/aspect/ValidationAspect.java
@@ -8,14 +8,14 @@ import org.springframework.core.annotation.Order;
 @Aspect
 @Order(2)
 public class ValidationAspect {
-    /***
-     * Following is a dummy implementation of this aspect.
-     * You are expected to provide an actual implementation based on the requirements, including adding/removing advices as needed.
-     */
+  /***
+   * Following is a dummy implementation of this aspect.
+   * You are expected to provide an actual implementation based on the requirements, including adding/removing advices as needed.
+   */
 
-	@Before("execution(public int edu.sjsu.cmpe275.aop.tweet.TweetService.retweet(..))")
-	public void dummyBeforeAdvice(JoinPoint joinPoint) {
-		System.out.printf("Permission check before the executuion of the metohd %s\n", joinPoint.getSignature().getName());
-	}
-	
+  @Before("execution(public int edu.sjsu.cmpe275.aop.tweet.TweetService.retweet(..))")
+  public void dummyBeforeAdvice(JoinPoint joinPoint) {
+    System.out.printf("Permission check before the executuion of the metohd %s\n", joinPoint.getSignature().getName());
+  }
+
 }
