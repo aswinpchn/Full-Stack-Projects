b41c3b813aea0cf6269685ccd13fc28e199caa92
Aswin <aswin-pc@MacBook-Pro.local>
2020-03-03 03:54:26 -0800

    StatsAspect: Added for block.

--- src/main/java/edu/sjsu/cmpe275/aop/tweet/aspect/StatsAspect.java
+++ src/main/java/edu/sjsu/cmpe275/aop/tweet/aspect/StatsAspect.java
@@ -10,6 +10,9 @@ import org.springframework.core.annotation.Order;
 
 import edu.sjsu.cmpe275.aop.tweet.TweetStatsServiceImpl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Aspect
 @Order(0)
 public class StatsAspect {
@@ -24,6 +27,22 @@ public class StatsAspect {
   public void dummyAfterAdvice(JoinPoint joinPoint) {
     System.out.printf("After the execution of the method %s in StatsAspect\n", joinPoint.getSignature().getName());
     //stats.resetStats();
+  }
+
+  @AfterReturning("execution(public * edu.sjsu.cmpe275.aop.tweet.TweetService.block(..))")
+  public void blockAdvice(JoinPoint joinPoint) {
+    System.out.printf("After the execution of the method %s in StatsAspect\n", joinPoint.getSignature().getName());
+
+    String userToBeBlocked = joinPoint.getArgs()[0].toString();
+    String userWhoIsBlocking = joinPoint.getArgs()[1].toString();
+
+    if(stats.block.get(userWhoIsBlocking) == null) {
+      Set<String> temp = new HashSet<String>();
+      temp.add(userToBeBlocked);
+      stats.block.put(userWhoIsBlocking, temp);
+    } else {
+      stats.block.get(userWhoIsBlocking).add(userToBeBlocked);
+    }
   }
 
 }
